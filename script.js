41a5bdea4b9ef8234d6b1252f1e1c0094dac12ad
Annabelle Fait <imannaf8@gmail.com>
2025-01-26 09:46:34 -0500

    Task 2 - Product Details

--- script.js
+++ script.js
@@ -6,3 +6,12 @@ var isActive = true; // A boolean value representing availability
 console.log("Employee Name", employeeName, typeof employeeName); // Log the Employee Name as a string
 console.log("employeeID", employeeID, typeof employeeID); // Log the EmployeeID as a Constant
 console.log("isActive", isActive, typeof isActive); // Log the Employee as Active True
+
+// Task 2 - Product Details
+let productName = "Product Name"; // String - Name of Product is Product Name
+const productPrice = 50; // Const - Make product price 50
+var isAvailable = true; // A boolean value indicating this product is available
+
+console.log("Product Name", productName, typeof productName); // Log the Product Name as a string
+console.log("Product Price", productPrice, typeof productPrice); // Log the Product Price as a const
+console.log("isActive", isActive, typeof isActive); // Log the Product as isActive True
