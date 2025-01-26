3e15df0238fcb79ba2fd2d4c473cab677ed5767d
Annabelle Fait <imannaf8@gmail.com>
2025-01-26 09:37:45 -0500

    Task 1 - Employee Information

---
+++ script.js
@@ -0,0 +1,8 @@
+// Task 1 - Employee Name
+let employeeName = "Employee Name"; // String - Name of Employee
+const employeeID = 0001; // const - Employee ID Number
+var isActive = true; // A boolean value representing availability
+
+console.log("Employee Name", employeeName, typeof employeeName); // Log the Employee Name as a string
+console.log("employeeID", employeeID, typeof employeeID); // Log the EmployeeID as a Constant
+console.log("isActive", isActive, typeof isActive); // Log the Employee as Active True
